eaa9dfc6a2917d509753a31fd9981e265d0f0646
Shahab Azimi <azimishahab@gmail.com>
2023-05-28 00:29:27 +0330

    Created Method for Reading Excel Files

---
+++ app/src/main/java/ir/shahabazimi/smsbomber/Constants.kt
@@ -0,0 +1,3 @@
+package ir.shahabazimi.smsbomber
+
+    const val EXCEL_MIME="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
--- app/src/main/java/ir/shahabazimi/smsbomber/MainActivity.kt
+++ app/src/main/java/ir/shahabazimi/smsbomber/MainActivity.kt
@@ -1,14 +1,69 @@
 package ir.shahabazimi.smsbomber
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.DocumentsContract
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toFile
+import ir.shahabazimi.smsbomber.databinding.ActivityMainBinding
+import org.apache.poi.ss.usermodel.WorkbookFactory
+import java.io.File
+import java.io.FileInputStream
 
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var photoPickerLauncher: ActivityResultLauncher<String>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+        initViews()
+    }
+
+    private fun init() {
+        photoPickerLauncher =
+            registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+                //todo get file path
+            }
+    }
+
+    private fun initViews() = with(binding) {
+        button.setOnClickListener { openExcelFile() }
+    }
+
+
+    private fun openExcelFile() = photoPickerLauncher.launch(
+        EXCEL_MIME
+    )
+
+    private fun readExcelFile(filepath: File) {
+        // val inputStream = FileInputStream(filepath)
+        //Instantiate Excel workbook using existing file:
+        val xlWb = WorkbookFactory.create(filepath)
+
+        //Row index specifies the row in the worksheet (starting at 0):
+        val rowNumber = 0
+        //Cell index specifies the column within the chosen row (starting at 0):
+        val columnNumber = 0
+
+        //Get reference to first sheet:
+        val xlWs = xlWb.getSheetAt(0)
+        Toast.makeText(
+            this,
+            xlWs.getRow(rowNumber).getCell(columnNumber).stringCellValue,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
 
